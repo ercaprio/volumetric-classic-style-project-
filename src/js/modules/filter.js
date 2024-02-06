e1db039c0085351f63298a658136bb8c31845389
Erkin <ercaprio@gmail.com>
2024-02-06 23:08:40 +0600

    code optimization filtering.js

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -1,19 +1,8 @@
 const filter = () => {
 	const menu = document.querySelector('.portfolio-menu'),
 		  items = menu.querySelectorAll('li'),
-		  btnAll = menu.querySelector('.all'),
-		  btnLovers = menu.querySelector('.lovers'),
-		  btnChef = menu.querySelector('.chef'),
-		  btnGirl = menu.querySelector('.girl'),
-		  btnGuy = menu.querySelector('.guy'),
-		  btnGrandmother = menu.querySelector('.grandmother'),
-		  btnGranddad = menu.querySelector('.granddad'),
 		  wrapper = document.querySelector('.portfolio-wrapper'),
 		  markAll = wrapper.querySelectorAll('.all'),
-		  markGirl = wrapper.querySelectorAll('.girl'),
-		  markLovers = wrapper.querySelectorAll('.lovers'),
-		  markChef = wrapper.querySelectorAll('.chef'),
-		  markGuy = wrapper.querySelectorAll('.guy'),
 		  no = document.querySelector('.portfolio-no');
 
 	const typeFilter = (markType) => {
@@ -36,33 +25,19 @@ const filter = () => {
 		}
 	};
 
-	btnAll.addEventListener('click', () => {
-		typeFilter(markAll);
-	});
+	const typeSelector = (type) => {
+		menu.querySelector(type).addEventListener('click', () => {
+			typeFilter(wrapper.querySelectorAll(type));
+		});
+	};
 
-	btnLovers.addEventListener('click', () => {
-		typeFilter(markLovers);
-	});
-
-	btnChef.addEventListener('click', () => {
-		typeFilter(markChef);
-	});
-
-	btnGirl.addEventListener('click', () => {
-		typeFilter(markGirl);
-	});
-
-	btnGuy.addEventListener('click', () => {
-		typeFilter(markGuy);
-	});
-
-	btnGrandmother.addEventListener('click', () => {
-		typeFilter();
-	});
-
-	btnGranddad.addEventListener('click', () => {
-		typeFilter();
-	});
+	typeSelector('.all');
+	typeSelector('.lovers');
+	typeSelector('.chef');
+	typeSelector('.girl');
+	typeSelector('.guy');
+	typeSelector('.grandmother');
+	typeSelector('.granddad');
 
 	menu.addEventListener('click', (e) => {
 		let target = e.target;
